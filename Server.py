1437d6288c12584f428d5dff9bf3d22485221781
Anand Dixit <52794800+ananddixit007@users.noreply.github.com>
2025-10-07 01:35:27 +0530

    Update Server.py
    Changed PORT as per the Render

--- Server.py
+++ Server.py
@@ -57,7 +57,8 @@ add_routes(
     path="/chain"
 )
 
-if __name__=="__main__":
-    import uvicorn
-    uvicorn.run(app,host="127.0.0.1",port=8000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Render provides PORT automatically
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
 
